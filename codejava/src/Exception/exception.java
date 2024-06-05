c0de8049950186c5d579ef4c845dfac844adabae
admin <admin@192.168.99.9>
2024-06-04 23:04:56 -0700

    Java Program

---
+++ codejava/src/Exception/exception.java
@@ -0,0 +1,29 @@
+package Exception;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class exception {
+
+	public static void main(String[] args)throws IOException
+	{
+
+		int a,b;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the value");
+		a=Integer.parseInt(br.readLine());
+		b=Integer.parseInt(br.readLine());
+		try
+		{
+			int c=a+b;
+			System.out.println("division:"+c);
+		}
+		catch(ArithmeticException sk)
+		{
+		System.out.println("Please entry the positive numbers");
+		System.out.println("NextLine");
+		}
+	}
+
+}
